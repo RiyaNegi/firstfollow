c1203273d4c6cf095bc53ae23408a3f16688e280
Riya <riyanegi221b@gmail.com>
2021-05-13 12:04:36 +0530

    add files

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/homepage")
+def index():
+    pass
+
+@app.route("/inputfromform",methods = ['POST','GET'])
+def savekeywords():
+    msg = ""
+    # global product_keys
+    if request.method == 'POST':
+        user = request.form['user']
+        keywords = request.form['keywords']
+        # product_keys = p_k
+        msg = "data added"
+        return redirect('/homepage')
+        
+    # print()
+    return render_template("app.html",msg = msg)
+
+if __name__ == "__main__":
+    app.run()
